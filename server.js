89d0e87bf68513524c33392d3cb0d9e655d22a3a
David Del Pozo <81963059+Daviddpc1992@users.noreply.github.com>
2021-07-19 23:23:33 +0200

    Produccion

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(dirname + "/dist"));
-
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(dirname + "/dist/index.html"));
+// HTTPS only middleware
+const forceSSL = function() { 
+    return function(req, res, next) {
+        if (req.headers['x-forwarded-proto'] !== 'https') {
+            return res.redirect(
+                ['https://', req.get('Host'), req.url].join('')
+            );
+        }
+        next();
+    }
+};
+app.use(forceSSL());
+// Actual host of the static Angular content
+app.use(express.static(__dirname + '/dist'));
+app.listen(process.env.PORT || 5000, function() {
+    console.log("Angular app running!");
 });
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
