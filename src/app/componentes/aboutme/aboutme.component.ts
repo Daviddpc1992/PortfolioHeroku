4dd20bba0b0e476c8642e9d2f3014e8700311bc7
David <daviddelpozocalderon@gmail.com>
2021-07-19 21:31:22 +0200

    Fixed v2

---
+++ src/app/componentes/aboutme/aboutme.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aboutme',
+  templateUrl: './aboutme.component.html',
+  styleUrls: ['./aboutme.component.css']
+
+  
+})
+  
+export class AboutmeComponent implements OnInit {
+
+  constructor() { }
+  
+
+  ngOnInit(): void {
+    
+  }
+  
+  
+  
+
+}
