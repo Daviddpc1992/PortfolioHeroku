e9e1b4d23e42ff2a240f54d159fd712935093948
David Del Pozo <81963059+Daviddpc1992@users.noreply.github.com>
2021-07-20 12:57:07 +0200

    fixed

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
+const express = require("express");
 
-const path = require('path');
-const express = require('express');
 const app = express();
-const server = http.createServer(process.env.PORT || 3000);
 
-// Serve static files
-app.use(express.static(__dirname + '/dist/portfolio'));
+app.use(express.static("./dist/portfolio"));
 
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/portfolio/index.html'));
+app.get("/*", function (req, res) {
+  res.sendFile("index.html", { root: "dist/portfolio" });
 });
+
+app.listen(process.env.PORT || 8080);
+
+console.log(`Running on port ${process.env.PORT || 8080}`);
