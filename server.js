83a6cfd8476bf51cabaf8e8f622a3aaf8f09cd4b
David <daviddelpozocalderon@gmail.com>
2021-07-20 12:08:54 +0200

    fixed

--- server.js
+++ server.js
@@ -11,3 +11,6 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/portfolio/index.html'));
 });
 
+http.listen(5000, function(){
+    console.log('listening on *:5000');
+  });
